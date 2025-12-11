04b223e79e9b67d3b93c7961c8d5f9a0aec6ddb4
Vitor Portes Mangueira <vitorportnoy@gmail.com>
2025-12-11 12:32:43 +0000

    fix(deps): altera CDN para esm.sh para corrigir erro de AuthClient

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const SUPABASE_URL = 'https://ruizerajoaoggfvkaadx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1aXplcmFqb2FvZ2dmdmthYWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzNjc5MTQsImV4cCI6MjA3NTk0MzkxNH0.bsphzec5g4xvs0lC5ivtzejY7tksjYu-iXIGAOTmRzE';
